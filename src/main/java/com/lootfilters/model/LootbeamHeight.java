786b9bc25c01e1ca02d291c2ba401b30840a0c01
riktenx <riktenx@gmail.com>
2025-12-08 13:47:49 -0500

    remove 8x lootbeam height since i guess that doesnt work

--- src/main/java/com/lootfilters/model/LootbeamHeight.java
+++ src/main/java/com/lootfilters/model/LootbeamHeight.java
@@ -5,17 +5,11 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum LootbeamHeight {
-    NORMAL("normal", 1),
-    TALL("tall", 2),
-    TALLER("taller", 4),
-    TALLEST("why?", 8);
+    NORMAL(1),
+    TALL(2),
+    TALLER(4),
+    ;
 
-    private final String name;
     @Getter
     private final int multiplier;
-
-    @Override
-    public String toString() {
-        return name;
-    }
 }
