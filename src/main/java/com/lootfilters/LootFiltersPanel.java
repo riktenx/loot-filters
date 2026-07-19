219232282667b7efcff53853fa72ee42decee35a
Kaqemeex <dev@kaqemeex.net>
2026-07-19 15:09:09 -0400

    Allow setting to null by user just not auto.

--- src/main/java/com/lootfilters/LootFiltersPanel.java
+++ src/main/java/com/lootfilters/LootFiltersPanel.java
@@ -194,9 +194,7 @@ public class LootFiltersPanel extends PluginPanel {
 
     private void onFilterSelect(ActionEvent event) {
         var selected = (String) filterSelect.getSelectedItem();
-        if (!NONE_ITEM.equals(selected)) {
-            plugin.setSelectedFilter(selected);
-        }
+	    plugin.setSelectedFilter(NONE_ITEM.equals(selected) ? null : selected);
     }
 
     private JButton createIconButton(BufferedImage icon, String tooltip, Runnable onClick) {
